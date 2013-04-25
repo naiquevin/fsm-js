6eed25a37dbc638e2476f3922d71177e5e038696
Vineet <naikvin@gmail.com>
2013-04-25 09:41:10 +0530

    Refactor the fsm implementation and add tests

--- src/fsm.js
+++ src/fsm.js
@@ -1,28 +1,53 @@
-var fsm = {
-    
-    start: function (callbacks, initArgs) {
+var fsm = function (callbacks) {
+    "use strict";
 
-        var result = callbacks.init.apply(null, initArgs);
-        var _stateName = result[1];
-        var _state = result[2];
+    var isInit = typeof (callbacks.init) === 'function';
+    var isHandleAll = typeof (callbacks.handleAll) === 'function';
 
-        return {
-            _stateName: _stateName,
-            _state: _state,
-            send: function (msg, data) {
-                var fn = callbacks[this._stateName]
-                var result = fn.apply(null, [msg, data, this._state]);
-                if (result[0] === 'reply') {
-                    var reply = result[1];
-                    this._stateName = result[2];
-                    _.extend(this._state, result[3]);
-                    return reply;
-                } else if (result[0] === 'next_state') {
-                    this._stateName = result[1];
-                    _.extend(this._state, result[2]);
-                    return null;
-                }
+    if (!isInit || !isHandleAll) {
+        throw new Error('FSM Error. One or more required callbacks not defined');
+    }
+
+    var getFunc = function (name) {
+        var fn = callbacks[name];
+        if (typeof (fn) !== 'function') {
+            throw new Error('Callback "' + name + '" not found');
+        }
+        return fn;
+    };
+
+    return {
+        start: function (initArgs) {
+            var fn = getFunc('init');
+            var result = fn.apply(null, initArgs);
+            this._stateName = result[1];
+            this._state = result[2];
+        },
+        send: function (msg, data) {
+            var fn = getFunc(this._stateName);
+            var result = fn.apply(null, [msg, data, this._state]);
+            return this._result(result);
+        },
+        send_all: function (msg, data) {
+            var fn = getFunc('handleAll');
+            var result = fn.apply(null, [msg, data, this._stateName, this._state]);
+            return this._result(result);
+        },
+        _result: function (result) {
+            switch (result[0]) {
+            case 'reply':
+                var reply = result[1];
+                this._stateName = result[2];
+                _.extend(this._state, result[3]);
+                return reply;
+            case 'next_state':
+                this._stateName = result[1];
+                _.extend(this._state, result[2]);
+                return null;
+            default:
+                throw new Error('Invalid reply');
             }
         }
-    }
+    };
 };
+
